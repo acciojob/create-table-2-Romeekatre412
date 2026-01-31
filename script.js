56f671500b5ff7005e7ccf4940103faa24e5d62e
Romee Katre <101556730+Romeekatre412@users.noreply.github.com>
2026-01-31 12:12:58 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,29 @@
-function createTable() {
-    //Write your code here
-  
+function createTable() 
+{
+	const table = document.getElementById("myTable");
+    table.innerHTML = "";
+    const rn = prompt("Input number of rows");
+    const cn = prompt("Input number of columns");
+    const rows = Number(rn);
+    const cols = Number(cn);
+    if (isNaN(rows) || isNaN(cols)) 
+	{
+        return;
+    }
+    if (rows <= 0 || cols <= 0) 
+	{
+        alert("Invalid input");
+        return;
+    }
+    for (let i = 0; i < rows; i++) 
+	{
+        const tr = document.createElement("tr");
+        for (let j = 0; j < cols; j++) 
+		{
+            const td = document.createElement("td");
+            td.innerHTML = `Row-${i} Column-${j}`;
+            tr.appendChild(td);
+        }
+        table.appendChild(tr);
+    }
 }
